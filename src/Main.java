954e7575757d5a70a3c96e0c4c1823d0bc1fcfa3
kozl4k <ketrab.leizok1@gmail.com>
2023-11-23 13:26:13 +0100

    skonczone zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków większy niż 2");
+        String znaki = scanner.nextLine();
+        while (znaki.length()<=2){
+            System.out.println("Podaj ciąg znaków większy niż 2");
+            znaki = scanner.nextLine();
+        }
+        String litery = znaki.substring((znaki.length()/2)-1,(znaki.length()/2)+1);
+        String first = znaki.substring(0,(znaki.length()/2)-1);
+        String last = znaki.substring((znaki.length()/2)+1);
+        String przeksztalcony = litery.concat(first.concat(last));
+        System.out.println(przeksztalcony);
+
+
+
+
+
+    }
 }
